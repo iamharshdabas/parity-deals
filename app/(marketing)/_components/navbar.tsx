5a186f17488bdb02c9e686ac641a423ab6968803
Harsh Dabas <iamharshdabas@gmail.com>
2024-10-27 16:58:59 +0530

    feat(config): add site icon

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,6 +1,5 @@
 import ThemeToggle from "@/components/theme/toggle";
 import { siteData, siteNavLinks } from "@/config/site";
-import { EarthIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Navbar() {
@@ -8,7 +7,8 @@ export default function Navbar() {
     <div className="flex fixed justify-center py-4 w-full">
       <header className="flex p-4 container rounded-2xl shadow-2xl border border-border justify-between items-center">
         <div className="flex gap-2 items-center">
-          <EarthIcon /> {siteData.title}
+          {siteData.icon}
+          {siteData.title}
         </div>
         <nav className="flex gap-2 items-center">
           {siteNavLinks.marketing.map((link) => (
--- config/site.tsx
+++ config/site.tsx
@@ -1,4 +1,7 @@
+import { Earth } from "lucide-react";
+
 export const siteData = {
+  icon: <Earth />,
   title: "Parity Deals",
   // TODO: Add description
   description: "",
