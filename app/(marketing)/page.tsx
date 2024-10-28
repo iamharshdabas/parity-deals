0f8c32036dc50c7ba79fbfee83830a6a6892348b
Harsh Dabas <iamharshdabas@gmail.com>
2024-10-28 11:59:42 +0530

    feat(marketing): add subscription tire

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { subtitle, title } from "@/config/class-variants";
+import {
+  subscriptionData,
+  subscriptionTires,
+} from "@/config/subscription-tire";
+import { CircleCheck } from "lucide-react";
+import numbro from "numbro";
 
 export default function Page() {
   return (
     <>
-      <section className="flex min-h-screen flex-col justify-center py-24 items-center gap-8">
+      <section className="flex min-h-screen flex-col justify-center py-16 items-center gap-8">
         <h1 className={title()}>Price Smarter, Sell bigger!</h1>
         <h2 className={subtitle({ class: "max-w-prose text-center" })}>
           Optimize your product pricing across countries to maximize sales.
@@ -15,6 +22,60 @@ export default function Page() {
           <Button>Get started for free </Button>
         </div>
       </section>
+
+      <section className="flex min-h-screen flex-col justify-center py-16 items-center gap-16">
+        <h1 className={title({ class: "text-center text-balance" })}>
+          {subscriptionData.title}
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 sm:gap-8 lg:gap-16">
+          {subscriptionTires.map((tire) => (
+            <Card key={tire.name}>
+              <CardHeader>
+                <CardTitle>{tire.name}</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-4">
+                <h1 className={title({ size: "sm" })}>
+                  <span>
+                    {numbro(tire.price).formatCurrency({
+                      thousandSeparated: true,
+                      mantissa: 2,
+                      spaceSeparated: true,
+                    })}
+                  </span>
+                  <span className={subtitle()}>/month</span>
+                </h1>
+                <h2 className={subtitle({ class: "flex gap-2 font-bold" })}>
+                  <span>
+                    {numbro(tire.maxNumberOfVisits).format({
+                      thousandSeparated: true,
+                      mantissa: 0,
+                      average: true,
+                    })}
+                  </span>
+                  <span> page visits</span>
+                </h2>
+                <Button className="w-full">Get started</Button>
+                <p className={subtitle({ class: "text-primary" })}>
+                  {tire.maxNumberOfProducts} product
+                  {tire.maxNumberOfProducts !== 1 && "s"}
+                </p>
+                {tire.canAccessAnalytics && <Feature title="Analytics" />}
+                {tire.canCustomizeBanner && <Feature title="Custom banner" />}
+                {tire.canRemoveBranding && <Feature title="Remove branding" />}
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </section>
     </>
+  );
+}
+
+function Feature({ title }: { title: string }) {
+  return (
+    <div className="flex gap-2">
+      <CircleCheck className="stroke-primary bg-primary/25 rounded-full" />
+      {title}
+    </div>
   );
 }
---
+++ config/subscription-tire.tsx
@@ -0,0 +1,54 @@
+type SubscriptionTire = {
+  name: string;
+  price: number;
+  maxNumberOfProducts: number;
+  maxNumberOfVisits: number;
+  canAccessAnalytics: boolean;
+  canCustomizeBanner: boolean;
+  canRemoveBranding: boolean;
+};
+
+const free: SubscriptionTire = {
+  name: "Free",
+  price: 0,
+  maxNumberOfProducts: 1,
+  maxNumberOfVisits: 5000,
+  canAccessAnalytics: false,
+  canCustomizeBanner: false,
+  canRemoveBranding: false,
+};
+
+const basic: SubscriptionTire = {
+  name: "Basic",
+  price: 19,
+  maxNumberOfProducts: 10,
+  maxNumberOfVisits: 10000,
+  canAccessAnalytics: true,
+  canCustomizeBanner: false,
+  canRemoveBranding: false,
+};
+
+const standard: SubscriptionTire = {
+  name: "Standard",
+  price: 49,
+  maxNumberOfProducts: 25,
+  maxNumberOfVisits: 100000,
+  canAccessAnalytics: true,
+  canCustomizeBanner: true,
+  canRemoveBranding: false,
+};
+
+const premium: SubscriptionTire = {
+  name: "Premium",
+  price: 99,
+  maxNumberOfProducts: 50,
+  maxNumberOfVisits: 1000000,
+  canAccessAnalytics: true,
+  canCustomizeBanner: true,
+  canRemoveBranding: true,
+};
+
+export const subscriptionTires = [free, basic, standard, premium];
+export const subscriptionData = {
+  title: "Pricing software which pays for itself 20x over",
+};
