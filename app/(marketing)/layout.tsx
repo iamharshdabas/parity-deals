28fd3f7fea52d419d2c51eb5bfa77581e2a97145
Harsh Dabas <iamharshdabas@gmail.com>
2024-11-04 20:20:42 +0530

    refactor: add root layout for consistant layout between marketing and other routes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,49 +1,13 @@
-import "@/app/globals.css";
-import ThemeProvider from "@/components/theme/provider";
-import { siteData } from "@/config/site";
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { ReactNode } from "react";
 import Navbar from "./_components/navbar";
 
-const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata: Metadata = {
-  title: siteData.title,
-  description: siteData.description,
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          <div className="container mx-auto px-4">
-            <main>{children}</main>
-          </div>
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      <Navbar />
+      <div className="container mx-auto px-4">
+        <main>{children}</main>
+      </div>
+    </>
   );
 }
---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import "@/app/globals.css";
+import ThemeProvider from "@/components/theme/provider";
+import { siteData } from "@/config/site";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { ReactNode } from "react";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: siteData.title,
+  description: siteData.description,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
