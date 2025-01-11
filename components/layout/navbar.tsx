ca7d2c7d3b9593efa05e24c313cb13043011ea32
Harsh Dabas <iamharshdabas@gmail.com>
2025-01-11 21:01:55 +0530

    fix: navbar layout

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -6,25 +6,27 @@ import NavLinks from "./nav-links";
 
 export default function Navbar() {
   return (
-    <header className="p-1 sm:p-4 container fixed w-full">
-      <div className="flex bg-background/80 backdrop-blur-lg p-2 sm:p-4 rounded-2xl shadow-2xl border border-border justify-between items-center">
-        <div className="flex gap-2 items-center">
-          {siteData.icon}
-          <span className="hidden sm:block">{siteData.title}</span>
+    <div className="fixed w-full flex justify-center">
+      <header className="p-1 sm:p-4 container">
+        <div className="flex bg-background/80 backdrop-blur-lg p-2 sm:p-4 rounded-2xl shadow-2xl border border-border justify-between items-center">
+          <div className="flex gap-2 items-center">
+            {siteData.icon}
+            <span className="hidden sm:block">{siteData.title}</span>
+          </div>
+          <nav className="flex gap-2 items-center">
+            <NavLinks links={siteNavLinksWithHome} />
+          </nav>
+          <div className="flex gap-2 items-center">
+            <ThemeToggle />
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
         </div>
-        <nav className="flex gap-2 items-center">
-          <NavLinks links={siteNavLinksWithHome} />
-        </nav>
-        <div className="flex gap-2 items-center">
-          <ThemeToggle />
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
